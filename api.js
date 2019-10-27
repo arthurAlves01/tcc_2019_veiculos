a3ba4ecf2d3dd907910bd6b271017abdd06ec52f
arthur <arthur.vdgad@gmail.com>
2019-10-27 17:53:17 -0300

    inicio implantação API

--- api.js
+++ api.js
@@ -1,22 +1,81 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const crud = require("./crud.js") //Funções do db que retornam promisses
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
-router.get("/busca/veiculo/item/:key/:fabricante/:modelo/:ano", (req,res) => {
+router.get("/busca/", (req,res) => {
+    //Teste da URL
+    //http://localhost/api/busca?key=teste&fabricante=honda&modelo=forza%20250&ano=2005
     res.setHeader('Content-Type', 'application/json');
-    let _key = req.params.key;
-    let _fab = req.params.fabricante;
-    let _mod = req.params.modelo;
-    let _ano = req.params.ano;
-    if(_key) {
-        res.json({"msg":"chave_invalida","errn":"1"})
+    let _apiKey = "djxwj3qr3usu34s";
+    let _key = req.query.key;
+    let _fab = req.query.fabricante;
+    let _mod = req.query.modelo;
+    let _ano = req.query.ano;
+    if(_key!=_apiKey) {
+        res.json({"msg":"chave_invalida","errn":"1"});
+        return;
     }
-    if(_fab) {
-        
+    if(_fab==undefined) {
+        //retornar lista de fabricantes
+        crud.buscaFabricantes()
+        .then(
+            (data) => {
+                res.send(data)
+            }
+        )
+        .catch(
+            (err) => {
+                res.json({"err":err,"errn":"2"})
+            }
+        )
+        return;
     }
+    if(_mod==undefined) {
+        //retornar lista de modelos da fabricante
+        crud.buscaModelos(_fab)
+        .then(
+            (data) => {
+                res.send(data)
+            }
+        )
+        .catch(
+            (err) => {
+                res.json({"err":err,"errn":"3"})
+            }
+        )
+        return;
+    }
+    if(_ano==undefined) {
+        //retornar lista com todos os anos de fabricação do modelo
+        crud.buscaAnomodelo(_fab,_mod)
+        .then(
+            (data) => {
+                res.send(data)
+            }
+        )
+        .catch(
+            (err) => {
+                res.json({"err":err,"errn":"4"})
+            }
+        )
+        return;
+    }
+    crud.buscaDadosVeiculo(_fab,_mod,_ano)
+        .then(
+            (data) => {
+                res.send(data)
+            }
+        )
+        .catch(
+            (err) => {
+                res.json({"err":err,"errn":"5"})
+            }
+        )
 })
-
+router.get("/*", (req,res) => {
+    res.json({"msg":"teste api"})
+})
 
 module.exports = router;
