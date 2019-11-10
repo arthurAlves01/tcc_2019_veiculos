e73b5a31d73bbbab7f2abf8dab7339f348734b6e
arthur <arthur.vdgad@gmail.com>
2019-11-09 23:46:26 -0300

    opções de menu

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const crud = require("./crud.js") //Funções do db que retornam promisses
 const session = require("express-session")
 const api = require("./api.js")
 const path = require("path")
+const c = require("crypto")
+
+
+function getSha256(v) {
+    let md5 = crypto.createHash("md5")
+    md5.update(v);
+    let _hash = md5.digest("hex")
+    return _hash;
+}
+
 
 app.set('view engine', 'pug')
 
@@ -23,7 +33,11 @@ app.use("/script", express.static("script"))
 app.use("/api", api)
 
 app.get("/login", (req,res) => {
-    res.render("login")
+    if(req.session.loggedin) {
+        res.redirect("/")
+    } else {
+        res.render("login")
+    }
 })
 
 app.get(["/","/*"], (req, res, next) => {
@@ -55,9 +69,48 @@ app.post("/login", (req,res) => {
 })
 
 app.get("/cadastraVeiculo", (req,res) => {
+    function campo(id, texto) {
+        this.id = id;
+        this.texto = texto;
+    }
+    let listaCampos = [
+        new campo("arqFoto", "Nome Arquivo Foto"),
+        new campo("nomeModelo", "Nome do Modelo"),
+        new campo("anoFabricacao", "Ano de Fabricação"),
+        new campo("cores", "Cores"),
+        new campo("tipoChassi", "Tipo Chassi"),
+        new campo("suspDianteira", "Suspenção Dianteira"),
+        new campo("suspTraseira", "Suspensão Traseira"),
+        new campo("pnDianteiro", "Pneu Dianteiro"),
+        new campo("pnTraseiro", "Pneu Traseiro"),
+        new campo("frDianteiro", "Freio Dianteiro"),
+        new campo("frTraseiro", "Freio Traseiro"),
+        new campo("tpFreio", "Tipo de Freio"),
+        new campo("qtdCilindros", "Quantidade de Cilindros"),
+        new campo("diametro", "Diâmetro"),
+        new campo("curso", "Curso"),
+        new campo("cilindrada", "Cilindrada"),
+        new campo("potMax", "Potência Máxima"),
+        new campo("tqMax", "Torque Máximo"),
+        new campo("stPartida", "Sistema de Partida"),
+        new campo("tpAlimentacao", "Tipo de Alimentação"),
+        new campo("combustivel", "Combustível"),
+        new campo("stTransmissao", "Sistema de Transmissão"),
+        new campo("cambio", "Cãmbio"),
+        new campo("bateria", "Bateria"),
+        new campo("txCompress", "Taxa de Compressão"),
+        new campo("comprimento", "Comprimento"),
+        new campo("largura", "Largura"),
+        new campo("altura", "Altura"),
+        new campo("distEixos", "Distância entre Eixos"),
+        new campo("distSolo", "Distância do Solo"),
+        new campo("altAs", "Altura do Assento"),
+        new campo("tqComb", "Tanque de Combustível"),
+        new campo("peso", "Peso")
+    ]
     crud.listaMontadoras()
         .then((data) => {
-            res.render("cadastraModelo", {montadoras: data, usuario: req.session.username})
+            res.render("cadastraModelo", {montadoras: data, usuario: req.session.username, campos: listaCampos})
         })
         .catch((err) => {
             res.send(err)
@@ -90,10 +143,10 @@ app.get("/listar/modelos", (req,res) => {
     })
 })
 
-
 app.get("/cadastraMontadora", (req,res) => {
     res.render("cadastraMontadora")
 })
+
 app.post("/add/montadora", (req,res) => {
     let montadora = req.body.nomeMontadora;
     crud.addMontadora(montadora)
@@ -105,7 +158,12 @@ app.post("/add/montadora", (req,res) => {
         })
 })
 
-//app.use(express.static(__dirname))
+app.get("/sair", (req,res) => {
+    req.session.destroy(() => {
+        res.redirect("/")
+    })
+})
+//app.use("/",) 404
 
 app.listen(80, function () {
   console.log('Example app listening on port 80!');
