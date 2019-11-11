02137f39fa49a604bab8fd6dc6e17833ac0d6ddf
arthur <arthur.vdgad@gmail.com>
2019-11-11 01:55:41 -0300

    erro ortográfico

--- crud.js
+++ crud.js
@@ -12,12 +12,13 @@ function getSha256(v) {
     return _hash;
 }
 
-//Cria senha aleatoria
-function makepw(length) {
+//Cria senha aleatoria com a quantidade de caracteres do parametro l
+//
+function makepw(l) {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    for ( var i = 0; i < l; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
